e70fc0837c697821cce1d0ee92e1fa6b2210ec86
jerceecallado <jercee.callado@gmail.com>
2019-03-15 14:33:41 +1030

    reactjs crash course following traversymedia

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the to do list app</p>
+      <p>version: 1.0.0</p>
+      <p>This is part of the React crash course by Traversy Media</p>
+    </React.Fragment>
+  );
+}
